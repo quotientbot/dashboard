2a165b079b98e7288fffd059b2ae238fad95a159
deadaf <gahlyanrohit18@gmail.com>
2023-06-30 21:41:04 +0530

    setup frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+	return <div className="bg-primary w-full overflow-hidden">App</div>;
+};
+
+export default App;
---
+++ frontend/src/constants/index.js
@@ -0,0 +1,19 @@
+export const navLinks = [
+    {
+      id: "home",
+      title: "Home",
+    },
+    {
+      id: "features",
+      title: "Features",
+    },
+    {
+      id: "premium",
+      title: "Premium",
+    },
+    {
+      id: "dashboard",
+      title: "Dashboard",
+    },
+  ];
+  
