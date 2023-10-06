3b4f7817a9f905162a811ef3cd3f619dcef79e9b
deadaf <gahlyanrohit18@gmail.com>
2023-10-06 22:37:57 +0530

    feat: create get mutual_guilds route

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends
 from app.utils import checks
+from app.config import DISCORD_API_ENDPOINT, BOT_TOKEN
+import httpx
 
 router = APIRouter()
 
@@ -7,3 +9,42 @@ router = APIRouter()
 @router.get("/@me")
 async def _me(user: dict = Depends(checks.get_user_details)):
     return user
+
+
+@router.get("/@me/guilds")
+async def get_user_guilds(user: dict = Depends(checks.get_user_details)):
+    discord_url = DISCORD_API_ENDPOINT + "/users/@me/guilds"
+    headers = {
+        "Authorization": f"Bearer {user['access_token']}",
+    }
+
+    # Get user's guilds
+    async with httpx.AsyncClient() as client:
+        response = await client.get(discord_url, headers=headers)
+        if response.status_code != 200:
+            return response.json()
+
+    user_guilds = response.json()
+
+    # Get bot's guilds
+    headers = {
+        "Authorization": f"Bot {BOT_TOKEN}",
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(discord_url, headers=headers)
+        if response.status_code != 200:
+            return response.json()
+
+    bot_guilds = response.json()
+
+    mutual_guilds = []
+    for user_guild in user_guilds:
+        for bot_guild in bot_guilds:
+            if (
+                user_guild["id"] == bot_guild["id"]
+                and (int(user_guild["permissions"]) & 0x00000020) == 0x00000020
+            ):
+                mutual_guilds.append(user_guild)
+
+    return mutual_guilds
