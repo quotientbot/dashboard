f03dde6bd6d2e88707af9af18f1da7668a156807
deadaf <gahlyanrohit18@gmail.com>
2023-10-06 18:35:15 +0530

    feat(orm): register tortoise

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
+from app import config
 from fastapi import FastAPI
 from .routers import oauth
+from tortoise.contrib.fastapi import register_tortoise
+
 
 app = FastAPI()
 app.include_router(oauth.router, prefix="/oauth")
@@ -8,3 +11,11 @@ app.include_router(oauth.router, prefix="/oauth")
 @app.get("/")
 async def root():
     return {"ping": "pong"}
+
+
+register_tortoise(
+    app,
+    config=config.TORTOISE,
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
