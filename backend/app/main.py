d291fc98376f213da612ea03ef37d2ddc842a0eb
deadaf <gahlyanrohit18@gmail.com>
2023-10-07 15:48:34 +0530

    feat: enable cors for all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,19 @@ from app import config
 from fastapi import FastAPI
 from .routers import oauth, user
 from tortoise.contrib.fastapi import register_tortoise
-
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
 app.include_router(oauth.router, prefix="/oauth")
 app.include_router(user.router, prefix="/users")
 
