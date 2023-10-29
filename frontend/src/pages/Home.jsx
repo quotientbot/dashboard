f9531cd860b232c13943d9fa974311645cd775c7
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-10-29 23:27:20 +0530

    file restructured

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
-import Commonbtn from '../components/core/Button'
+import Commonbtn from '../components/common/Button'
 import logo from "../assets/favicon.png"
-import Footer from '../components/core/Footer'
+import Footer from '../components/common/Footer'
 
 const Home = () => {
 
