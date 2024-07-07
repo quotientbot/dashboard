0a8572da0a29b9dd13277c0e950b82a31fa8b93a
vishwakant1602 <121593799+vishwakant1602@users.noreply.github.com>
2024-07-07 14:33:32 +0530

    Improve Privacy Policy page and added routes in footerEnhance Privacy Policy page and update Footer component
    - Improved readability and structure of Privacy Policy page with clear sections and content.
    - Added new routes to the Footer component for Contact Us, Invite, and Support.
    - Updated Footer component responsiveness using Chakra UI's responsive utilities.

--- frontend/src/components/home/Footer.jsx
+++ frontend/src/components/home/Footer.jsx
@@ -15,79 +15,78 @@ const Footer = ({delay}) => {
         initial = {{y : +100}}
         animate = {{y : 0}}
         transition={{delay : delay, staggerChildren: 0.2}}
-        className=' w-full lg:h-[7vh] h-[10vh] font-medium flex  flex-wrap justify-evenly lg:gap-0 gap-[5vw] items-center duration-500 ease-in-out text-lg lg:pt-1'>
-
-{/* <Box>
-
-
-<Stack direction={["column", "row"]} h={"full"} alignItems={"center"} color={"white"}>
-  <VStack w={"full"} alignItems={["center", "flex-start"]}>
-    <Text fontWeight={"bold"}>About Us</Text>
-    <Text
-      fontSize={"sm"}
-      letterSpacing={"widest"}
-      textAlign={["center", "left"]}
-    >
-     Get all information about Cryto from us.
-     <br />
-     Happy to serve
-    </Text>
-  </VStack>
-
-  <VStack>
-    <Avatar boxSize={"28"} mt={["4", "0"]}  bg="twitter.900" />
-    <Text>Our Founder</Text>
-    <Text mt={-4}>- Monic</Text>
-  </VStack>
-</Stack>
-</Box> */}
+        className=' w-full lg:h-[7vh] h-[10vh] font-medium flex  flex-wrap justify-evenly lg:gap-0 gap-[5vw] items-center duration-500 ease-in-out text-lg lg:pt-1'
+        >
     
 
-            <NavLink to={"/contact"} className={({ isActive }) =>
-                isActive
-                    ? "underline underline-offset-4 font-bold"
-                    : "hover:underline underline-offset-4"
-            } >Copyright © 2022 | Quotient</NavLink>
+    <NavLink
+        to={"/"}
+        className={({ isActive }) =>
+          isActive
+            ? "underline underline-offset-4 font-bold"
+            : "hover:underline underline-offset-4"
+        }
+      >
+        Copyright © 2022 | Quotient
+      </NavLink>
 
-            <NavLink to={"https://discord.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fclient_id%3D746348747918934096%26scope%3Dapplications.commands%2520bot%26permissions%3D536737213566"} className={({ isActive }) =>
-                isActive
-                    ? "underline underline-offset-4 font-bold"
-                    : "hover:underline underline-offset-4"
-            } >Invite</NavLink>
+      <NavLink
+        to={"https://discord.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fclient_id%3D746348747918934096%26scope%3Dapplications.commands%2520bot%26permissions%3D536737213566"}
+        className={({ isActive }) =>
+          isActive
+            ? "underline underline-offset-4 font-bold"
+            : "hover:underline underline-offset-4"
+        }
+      >
+        Invite
+      </NavLink>
 
-            <NavLink to={"/*"} className={({ isActive }) =>
-                isActive
-                    ? "underline underline-offset-4 font-bold"
-                    : "hover:underline underline-offset-4"
-            } >Support</NavLink>
+      <NavLink
+        to={"/contact"}
+        className={({ isActive }) =>
+          isActive
+            ? "underline underline-offset-4 font-bold"
+            : "hover:underline underline-offset-4"
+        }
+      >
+        Support
+      </NavLink>
 
-            <NavLink to={"/contact"} className={({ isActive }) =>
-                isActive
-                    ? "underline underline-offset-4 font-bold"
-                    : "hover:underline underline-offset-4"
-            } >T&C</NavLink>
+      <NavLink
+        to={"/*"}
+        className={({ isActive }) =>
+          isActive
+            ? "underline underline-offset-4 font-bold"
+            : "hover:underline underline-offset-4"
+        }
+      >
+        T&C
+      </NavLink>
 
-            <NavLink to={"/policy"} className={({ isActive }) =>
-                isActive
-                    ? "underline underline-offset-4 font-bold"
-                    : "hover:underline underline-offset-4"
-            } >Privacy Policy</NavLink>
+      <NavLink
+        to={"/policy"}
+        className={({ isActive }) =>
+          isActive
+            ? "underline underline-offset-4 font-bold"
+            : "hover:underline underline-offset-4"
+        }
+      >
+        Privacy Policy
+      </NavLink>
 
-            <NavLink to={"/contact"} className={({ isActive }) =>
-                isActive
-                    ? "underline underline-offset-4 font-bold"
-                    : "hover:underline underline-offset-4"
-            } >Refund Policy</NavLink>
+      <NavLink
+        to={"/contact"}
+        className={({ isActive }) =>
+          isActive
+            ? "underline underline-offset-4 font-bold"
+            : "hover:underline underline-offset-4"
+        }
+      >
+        Refund Policy
+      </NavLink>
+    </motion.div>
+  );
+};
 
-
-          
-            
-        </motion.div>
-       
-    )
-}
-
-export default Footer
-
-
+export default Footer;
 
